6d65d927becb246ba462bbfaaab66d1948d24667
Ghost Dev <ntrongquy2003@gmail.com>
2025-03-18 13:54:18 +0700

    commit FE tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="bg-blue-500 p-4">
+        <h1 className="text-2x1 font-bold text-white">Xin Chào</h1>
+        <button className="bg-green-500 text-white px-2 py-1 rounded">
+          Click
+        </button>
+      </div>
     </div>
   );
 }
